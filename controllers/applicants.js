d4ee918fd8837249d5b03fdd8e0262aa4e3956c6
Lia <liacicati@gmail.com>
2021-04-27 17:20:35 +0200

    feat : added controllers

---
+++ controllers/applicants.js
@@ -0,0 +1,29 @@
+const Applicant = require('../models/applicant');
+const BadRequestError = require('../errors/BadRequestError');
+const ConflictError = require('../errors/ConflictError');
+
+const addData = (req, res, next) => {
+  const {
+    email, phone, firstName, lastName,
+  } = req.body;
+  if (!email || !phone || !firstName || !lastName) {
+    throw new BadRequestError('Some of the fields are empty');
+  }
+
+  const applicantData = new Applicant(req.body);
+  applicantData.save()
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.code === 11000) {
+        next(new ConflictError('A user with this email is already registered in the database'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports = {
+  addData,
+};
---
+++ controllers/beneficiaries.js
@@ -0,0 +1,29 @@
+const Beneficiar = require('../models/beneficiar');
+const BadRequestError = require('../errors/BadRequestError');
+const ConflictError = require('../errors/ConflictError');
+
+const addData = (req, res, next) => {
+  const {
+    email, phone, firstName, lastName,
+  } = req.body;
+  if (!email || !phone || !firstName || !lastName) {
+    throw new BadRequestError('Some of the fields are empty');
+  }
+
+  const beneficiarData = new Beneficiar(req.body);
+  beneficiarData.save()
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.code === 11000) {
+        next(new ConflictError('A user with this email is already registered in the database'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports = {
+  addData,
+};
